b811b6e6d6c97379061cf7f45042c9a72f333722
Gustavo Messagi <gustavo.ramos.messagi@gmail.com>
2020-11-28 14:42:44 -0300

    feat: token verification before create an announcement

--- src/Controllers/AnnouncementController.ts
+++ src/Controllers/AnnouncementController.ts
@@ -1,8 +1,28 @@
 import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 import PetController from './PetController';
 import AnnouncementModel from '../Model/Announcement';
 
+dotenv.config();
+
+const privateJwtKey = process.env.PRIVATE_JWT_KEY || '';
+
 const create = async (req: Request, res: Response) => {
+  const token = req.headers.authorization?.split(' ')[1] || '';
+
+  console.log(token);
+
+  if (!token) {
+    res.status(401).send('Usuário não autenticado.');
+  }
+
+  jwt.verify(token, privateJwtKey, function (err) {
+    if (err) {
+      res.status(401).send('Token não válido');
+    }
+  });
+
   const { pets, userId, announcement } = req.body;
 
   const createdPets = await Promise.all(pets.map(async (pet: any) => {
