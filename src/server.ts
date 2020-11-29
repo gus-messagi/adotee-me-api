1e95c7283c263bba48fbe913ed42e4223231730e
Gustavo Messagi <gustavo.ramos.messagi@gmail.com>
2020-11-28 22:52:40 -0300

    feat: added cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import swaggerUi from 'swagger-ui-express';
 import routes from './routes';
@@ -8,6 +9,8 @@ import * as swaggerDocument from './swagger.json';
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
 
 const url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@getpet-cluster.1lm9b.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`;
 
